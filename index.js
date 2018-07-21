7785b0661555ff5d72417af4242062a6ea867410
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-07-22 03:43:00 +0900

    ドロップアイテム数確認できるよう変更

--- index.js
+++ index.js
@@ -77,6 +77,48 @@ controller.hears([MONSTER_NAME.properties[MONSTER_NAME.DESERT_FOGAN].category] ,
   }
 });
 
+controller.hears([MONSTER_NAME.properties[MONSTER_NAME.AKUMAN_TEMPLE].category] , ['direct_message'], (bot,message) =>{
+
+  let monsterCategory = MONSTER_NAME.properties[MONSTER_NAME.AKUMAN_TEMPLE].category;
+  let dropItemNum = message.text.split("\n")[1];
+
+  try{
+    let findByMonsterCategoryId = "SELECT * FROM ITEM WHERE MONSTER_CATEGORY_ID = (SELECT id FROM MONSTER_CATEGORY WHERE CATEGORY = ?)";
+    con.query(findByMonsterCategoryId,[monsterCategory],function(err,result,fields){
+      if(err) throw err;
+      let itemId = result[0].id;
+
+      if(dropItemNum === undefined || dropItemNum === null){
+        //TODO 月毎実装
+        let findById = "SELECT SUM(NUMBER) as sum FROM DROP_ITEMS WHERE ITEM_ID = ?";
+        con.query(findById,[itemId],function(err,result,fields){
+          if(err) throw err;
+          let sum = result[0].sum;
+          if(sum === undefined || sum === null){
+            bot.reply(message, monsterCategory + "のドロップ数は0です");
+          }
+          else{
+            bot.reply(message, monsterCategory + "のドロップ数は" + sum + "です");
+          }
+        });
+
+      }else{
+
+        let insert = "insert into drop_items (item_id,number,date) values(?,?,?)";
+        let now = new Date();
+        let date = now.toFormat('YYYY-MM-DD');
+        con.query(insert,[itemId,dropItemNum,date],function(err,rows,fields) {
+          if(err) throw err;
+          bot.reply(message,monsterCategory + "、ドロップアイテム数" + dropItemNum + "個で記録しました");
+        });
+      }
+    });
+  }catch(err){
+    console.log(err);
+    bot.reply(message,"処理中にエラーが発生しました");
+  }
+});
+
 controller.hears(["(.*)"], ['direct_message'], (bot,message) =>{
 	bot.reply(message,"*help or ヘルプ*\nを参照してください");
 });
