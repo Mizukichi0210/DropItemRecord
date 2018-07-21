2d19595f940e3558b6e2b4b4c342826d7b2ad621
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-07-22 03:31:45 +0900

    ドロップアイテム個数確認

--- index.js
+++ index.js
@@ -42,14 +42,27 @@ controller.hears([MONSTER_NAME.properties[MONSTER_NAME.DESERT_FOGAN].category] ,
   let dropItemNum = message.text.split("\n")[1];
 
   try{
-    let findByMonsterCategory = "SELECT * FROM MONSTER_CATEGORY WHERE CATEGORY = ?";
-    con.query(findByMonsterCategory,[monsterCategory],function(err,result,fields){
+    let findByMonsterCategoryId = "SELECT * FROM ITEM WHERE MONSTER_CATEGORY_ID = (SELECT id FROM MONSTER_CATEGORY WHERE CATEGORY = ?)";
+    con.query(findByMonsterCategoryId,[monsterCategory],function(err,result,fields){
       if(err) throw err;
-      let monsterCategoryId = result[0].id;
-      let findByMonsterCategoryId = "SELECT * FROM ITEM WHERE MONSTER_CATEGORY_ID = ?";
-      con.query(findByMonsterCategoryId,[monsterCategoryId],function(err,result,fields){
-        if(err) throw err;
-        let itemId =  result[0].id;
+      let itemId = result[0].id;
+
+      if(dropItemNum === undefined || dropItemNum === null){
+        //TODO 月毎実装
+        let findById = "SELECT SUM(NUMBER) as sum FROM DROP_ITEMS WHERE ITEM_ID = ?";
+        con.query(findById,[itemId],function(err,result,fields){
+          if(err) throw err;
+          let sum = result[0].sum;
+          if(sum === undefined || sum === null){
+            bot.reply(message, monsterCategory + "のドロップ数は0です");
+          }
+          else{
+            bot.reply(message, monsterCategory + "のドロップ数は" + sum + "です");
+          }
+        });
+
+      }else{
+
         let insert = "insert into drop_items (item_id,number,date) values(?,?,?)";
         let now = new Date();
         let date = now.toFormat('YYYY-MM-DD');
@@ -57,15 +70,12 @@ controller.hears([MONSTER_NAME.properties[MONSTER_NAME.DESERT_FOGAN].category] ,
           if(err) throw err;
           bot.reply(message,monsterCategory + "、ドロップアイテム数" + dropItemNum + "個で記録しました");
         });
-      });
-	  });
+      }
+    });
   }catch(err){
-    console.log(err);
     bot.reply(message,"処理中にエラーが発生しました");
   }
 });
-
 controller.hears(["(.*)"], ['direct_message'], (bot,message) =>{
 	bot.reply(message,"*help or ヘルプ*\nを参照してください");
 });
-
