ff39fb3485c41f9b10a19fcfbf0cb8df6b33311f
Mizukichi0210 <canaryyellow.nl@gmail.com>
2018-09-10 03:05:40 +0900

    登録する処理をregisterにまとめた

--- src/index.js
+++ src/index.js
@@ -16,69 +16,14 @@ controller.spawn({
 }).startRTM();
 
 /**
- * モンスターカテゴリ設定
- * @readonly
- * @enum
+ * ドロップ数記録 / ドロップ数確認
  */
-const MONSTER_NAME = {
+controller.hears(["(.*)"] , ['direct_message'], (bot,message) =>{
 
-  DESERT_FOGAN:1,
-  AKUMAN_TEMPLE:2,
-  FILA_KU : 3,
-
-  properties:{
-    1:{ category : "砂漠フォガン"},
-    2:{ category : "アクマン寺院"},
-    3:{ category : "フィラ・ク"},
-  }
-};
-
-/**
- * 砂漠フォガンのドロップ数記録 / ドロップ数確認
- */
-controller.hears([MONSTER_NAME.properties[MONSTER_NAME.DESERT_FOGAN].category] , ['direct_message'], (bot,message) =>{
-
-  let monsterCategory = MONSTER_NAME.properties[MONSTER_NAME.DESERT_FOGAN].category;
-  let dropItemNum = message.text.split("\n")[1];
-  let register = registerModule(monsterCategory,dropItemNum,bot,message);
+  let register = registerModule(bot,message);
   try{
     async.series(register,(err) =>{
 
-    });
-  }catch(err){
-    log(err);
-    bot.reply(message,"処理中にエラーが発生しました");
-  }
-});
-
-/**
- * アクマン寺院のドロップ数記録 / ドロップ数確認
- */
-controller.hears([MONSTER_NAME.properties[MONSTER_NAME.AKUMAN_TEMPLE].category] , ['direct_message'], (bot,message) =>{
-
-  let monsterCategory = MONSTER_NAME.properties[MONSTER_NAME.AKUMAN_TEMPLE].category;
-  let dropItemNum = message.text.split("\n")[1];
-  let register = registerModule(monsterCategory,dropItemNum,bot,message);
-  try{
-    async.series(register,(err) =>{
-      if(err) throw err;
-    });
-  }catch(err){
-    log(err);
-    bot.reply(message,"処理中にエラーが発生しました");
-  }
-});
-
-/**
- * フィラ・クのドロップ数記録 / ドロップ数確認
- */
-controller.hears([MONSTER_NAME.properties[MONSTER_NAME.FILA_KU].category] , ['direct_message'], (bot,message) =>{
-
-  let monsterCategory = MONSTER_NAME.properties[MONSTER_NAME.FILA_KU].category;
-  let dropItemNum = message.text.split("\n")[1];
-  let register = registerModule(monsterCategory,dropItemNum,bot,message);
-  try{
-    async.series(register,(err) =>{
     });
   }catch(err){
     log(err);
